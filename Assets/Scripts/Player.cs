66fde9f8f4c4be58cedb4ea796d3b3941b8969b5
Husar Andrei <dumitruandrei367@gmail.com>
2024-06-03 10:40:53 +0300

    Miau
    Adaugat la script rotatia si adaugat 3 efecte noi in post processing(l am facut mai rece)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-    void Start()
+    [SerializeField] private int MovSpeed = 1;
+    [SerializeField] private float rotateSpeed = 5f;
+    private void Update()
     {
-        
-    }
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
 
-    
-    void Update()
-    {
+        inputVector = inputVector.normalized;
+
+        Vector3 movDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        transform.position += movDir * (MovSpeed * Time.deltaTime);
+        transform.forward = Vector3.Slerp(transform.forward, movDir, Time.deltaTime * rotateSpeed);
         
     }
 }
