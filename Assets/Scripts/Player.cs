de114d7adb2c4dce76aa963251293a47deeade78
Husar Andrei <dumitruandrei367@gmail.com>
2024-06-04 11:32:14 +0300

    Ultim

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,47 @@ public class Player : MonoBehaviour
     [SerializeField] private GameInput _gameInput;
     [SerializeField] private int MovSpeed = 1;
     [SerializeField] private float rotateSpeed = 5f;
+    [SerializeField] private float _playerRadius = .7f;
+    [SerializeField] private float _playerHeight = 2f;
     public bool isWalking;
     private void Update()
     {
+        
         Vector2 input = _gameInput.GetMovementVectorNormalized();
         Vector3 movDir = new Vector3(input.x, 0f, input.y);
+        
+        var moveDistance = MovSpeed * Time.deltaTime;
+        var step = movDir * moveDistance;
+        
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * _playerHeight, _playerRadius, movDir, moveDistance);
+        
         isWalking = movDir != Vector3.zero;
-        transform.position += movDir * (MovSpeed * Time.deltaTime);
+        if (!canMove)
+        {
+            var moveDirX = new Vector3(movDir.x, 0, 0); 
+            bool canMoveX = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * _playerHeight, _playerRadius, moveDirX, moveDistance);
+            if (canMoveX)
+            {
+                canMove = true;
+                step = moveDirX * moveDistance;
+            }
+
+            if (!canMoveX)
+            {
+                var moveDirZ = new Vector3(0, 0, moveDirX.y);
+                bool canMoveZ = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * _playerHeight, _playerRadius, moveDirZ, moveDistance);
+                if (canMoveZ)
+                {
+                    canMove = true;
+                    step = moveDirZ * moveDistance;
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += step;
+        }
+
         transform.forward = Vector3.Slerp(transform.forward, movDir, Time.deltaTime * rotateSpeed);
         
     }
