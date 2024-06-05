632034ed020c668063578336e92dd9e53f706433
Husar Andrei <dumitruandrei367@gmail.com>
2024-06-05 10:24:40 +0300

    Terminat schimbarea de camere

---
+++ Assets/Scripts/cameraTrigg.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class cameraTrigg : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera room1;
+    [SerializeField] private CinemachineVirtualCamera room2;
+
+    private void Start()
+    {
+        Physics.IgnoreLayerCollision(0,8);
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (room1.enabled == true)
+        {
+            room1.enabled = false;
+            room2.enabled = true;
+        }
+        else
+        {
+            room2.enabled = false;
+            room1.enabled = true;
+        }
+    }
+}
